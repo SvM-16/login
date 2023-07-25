2010d94330d0eefb13dca97728f29c8a9d741c37
sebastian Vallejo Montoya <svallejo269@gmail.com>
2023-07-25 15:52:13 -0500

    login terminado

--- src/controllers/aut.controllers.js
+++ src/controllers/aut.controllers.js
@@ -1,24 +1,33 @@
 import User from '../models/user.models.js';
+import  bcrypt from 'bcryptjs'
+import { createTokenAcess } from '../libs/jwt.js';
 
-export const register = await (req, res) => {
+export const register = async (req, res) => {
     const {email, password, username} = req.body;
 
     // console.log(email, password, username);
     // res.send('Resgistrado');
 
     try {
-        
+        const passwordHash = await bcrypt.hash(password, 10)
 
         const newUser = new User ({
             username,
             email,
-            password
+            password:passwordHash
         });
 
-        const userSved = await
+        const userSaved = await newUser.save();
+        const token = await createTokenAcess({ id: userSaved._id });
+        res.cookie('token', token)
+        res.status(201).json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email
+        });
 
     } catch (error) {
-        res.status(500).json({massage: erro.message })
+        res.status(500).json({massage: error.message })
     }
 
 }
