84376565633bf29d410e64aabadae8357aea97fa
sebastian Vallejo <sebastianvallejo084@gmail.com>
2023-07-28 08:31:50 -0500

    avance de login

--- src/controllers/aut.controllers.js
+++ src/controllers/aut.controllers.js
@@ -31,4 +31,32 @@ export const register = async (req, res) => {
     }
 
 }
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const userFound = await User.findOne ({ email });
+        if(!userFound) return res.status(400).json({message : "User not found"});
+
+        const isMach = await bcrypt.compare(password, userFound.password);
+        if(!isMach) return res.status(400).json({massage: "Error in credentails"});
+
+        const token = await createTokenAcess({ id: userFound._id });
+        res.cookie('token', token)
+        res.status(201).json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+        });
+    } catch (error) {
+        res.status(500).json({massage: error.message })
+    }
+}
+
+
+export const logout = (req, res) =>{
+    res.cookie('token', '', {
+        expires : new Date(0),
+    });
+    return res.sendStatus(200);
+}
